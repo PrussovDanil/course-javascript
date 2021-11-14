5ad5100d183243d54e83a0691b2200a84c6abd1c
Danil Prussov <danilprussov@gmai.com>
2021-11-14 23:55:58 +0600

    end

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,10 +9,16 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
+
+
 
  Напишите аналог встроенного метода map для работы с массивами
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
@@ -20,7 +26,13 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const array_copy = [];
+  for (let i = 0; i < array.length; i++) {
+    array_copy[i] = fn(array[i], i, array);
+  }
+  return array_copy;
+}
 
 /*
  Задание 3:
@@ -31,7 +43,15 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let sum = initial || array[0],
+    i = initial ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    sum = fn(sum, array[i], i, array);
+  }
+  return sum;
+}
 
 /*
  Задание 4:
@@ -41,7 +61,14 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const arr = [];
+  for (let key in obj) {
+    key = key.toUpperCase();
+    arr.push(key);
+  }
+  return arr;
+}
 
 /*
  Задание 5 *:
@@ -54,6 +81,13 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set(obj, key, value) {
+      obj[key] = value ** 2;
+      return true;
+    },
+  });
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
